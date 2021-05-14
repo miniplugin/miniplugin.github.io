cad6dae164cc7e0003f4fd5bad1e65f3e5918e35
boramcom@daum.net <boramcom@daum.net>
2021-05-14 15:41:06 +0900

    6교시 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e126dd88c8a20eabde3c7",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Atkins Palmer"
+    },
+    {
+      "_id": "609e126dd2c4e68b72c8b01f",
+      "age": 36,
+      "eyeColor": "brown",
+      "name": "Emily Trujillo"
+    },
+    {
+      "_id": "609e126ddd5e8186c1312b8a",
+      "age": 22,
+      "eyeColor": "green",
+      "name": "Ursula Weaver"
+    },
+    {
+      "_id": "609e126d899f69db91d6b1ca",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Day Witt"
+    },
+    {
+      "_id": "609e126db8ca82a0cd0847d5",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "Pamela Knowles"
+    },
+    {
+      "_id": "609e126df430ec4e3145b1e1",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Irma Vasquez"
+    },
+    {
+      "_id": "609e126d4e12c9fef41c19ec",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Guy Sheppard"
+    },
+    {
+      "_id": "609e126de4428294a6a28950",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Bray Cross"
+    },
+    {
+      "_id": "609e126d476bd2c18c574199",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Holman Cummings"
+    },
+    {
+      "_id": "609e126d5d514a0df2490b66",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Claudia Miles"
+    },
+    {
+      "_id": "609e126d328eb801053c2f86",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Vasquez Gregory"
+    },
+    {
+      "_id": "609e126d139a7cab43b7d6bc",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Scott Slater"
+    },
+    {
+      "_id": "609e126d7d3cc1f74a2eebaf",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Rosario Ayala"
+    },
+    {
+      "_id": "609e126d8979530b5168cf76",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Beth Shannon"
+    },
+    {
+      "_id": "609e126dabd336ff8dde21a0",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Wendy Dodson"
+    },
+    {
+      "_id": "609e126dcf9503e8f6b8e33b",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Dorthy Fuller"
+    },
+    {
+      "_id": "609e126d295254d4db97e7a9",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Carly Potts"
+    },
+    {
+      "_id": "609e126dc1ac917cf6826977",
+      "age": 22,
+      "eyeColor": "green",
+      "name": "English Juarez"
+    },
+    {
+      "_id": "609e126d4f179e9778947a59",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Lyons Glass"
+    },
+    {
+      "_id": "609e126d7513f14f0cd985f3",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Harvey Miller"
+    }
+  ]
